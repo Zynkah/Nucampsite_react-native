7966455018beba51c82329b80392d41f8cf8ef48
Zynkah <ffgurl07@yahoo.com>
2022-11-09 09:24:44 -0800

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import { ScrollView } from "react-native";
 import { Card, ListItem, Avatar, Text } from "react-native-elements";
+import Loading from "../components/LoadingComponent";
 
 const Mission = () => {
   return (
@@ -24,6 +25,30 @@ const Mission = () => {
 const AboutScreen = () => {
   const partners = useSelector((state) => state.partners);
 
+  if (partners.isLoading) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Loading />
+        </Card>
+      </ScrollView>
+    );
+  }
+  if (partners.errMess) {
+    return (
+      <ScrollView>
+        <Mission />
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Text>{partners.errMess}</Text>
+        </Card>
+      </ScrollView>
+    );
+  }
   return (
     <ScrollView>
       <Mission />
